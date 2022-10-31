fb4e0d5a04f168ff2fb2e6563f10f27e3f12d767
4190 <lukasz.dab4@gmail.com>
2022-10-31 18:51:25 +0100

    initial commit
    Signed-off-by: 4190 <lukasz.dab4@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home/Home.component.tsx';
+import TestComponent from './pages/test/TestComponent.tsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" >Home</Link>
+            </li>
+            <li>
+              <Link to="/test" >test</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/test" element={<TestComponent/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
